ffbb465b55119f4ddbd78b3d8d030ff07197bf0c
RICARDO ARDI GUNAWAN <ricardo.gunawan@binus.ac.id>
2023-12-15 08:27:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react';
+import { marked } from 'marked';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons'
+import { faMaximize } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+  const [display, setDisplay] = useState(`
+  # I don't love you
+  ## Like i did yesterday
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+  - First item
+  - Second item
+  - Third item
+  > blockquote
+  ![alt text](image.jpg)
+  **bold text**
+  `)
+
+  marked.setOptions({
+    breaks:true
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className='text-editor'>
+          <div className='toolbar'><FontAwesomeIcon icon={faFreeCodeCamp} className='highlight'/> Editor<FontAwesomeIcon className='max-icon' icon={faMaximize} /></div> 
+          <textarea id="editor" value={display} onChange={(event)=> {setDisplay(event.target.value);}}></textarea>
+        </div>
+        <div className='box-preview'>
+        <div className='text-preview'>
+        <div className='toolbar'><FontAwesomeIcon icon={faFreeCodeCamp} className='highlight'/> Previewer<FontAwesomeIcon className='max-icon' icon={faMaximize} /></div> 
+        <div id="preview" dangerouslySetInnerHTML={{__html: marked(display)}}></div>
+        </div>
+        </div>
       </header>
     </div>
   );
